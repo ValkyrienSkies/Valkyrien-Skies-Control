dd7a3b3ca022f7f9b32d699b3f679c905c22162c
Rubydesic <rubydesic@gmail.com>
2020-09-18 19:02:05 -0400

    wrench modes displays accurate keys

--- src/main/java/org/valkyrienskies/addon/control/item/ItemVSWrench.java
+++ src/main/java/org/valkyrienskies/addon/control/item/ItemVSWrench.java
@@ -1,12 +1,10 @@
 package org.valkyrienskies.addon.control.item;
 
-import java.util.List;
-import javax.annotation.Nullable;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
@@ -16,18 +14,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import org.valkyrienskies.addon.control.block.multiblocks.GiantPropellerMultiblockSchematic;
-import org.valkyrienskies.addon.control.block.multiblocks.IMultiblockSchematic;
 import org.valkyrienskies.addon.control.block.multiblocks.ITileEntityMultiblockPart;
-import org.valkyrienskies.addon.control.block.multiblocks.RudderAxleMultiblockSchematic;
-import org.valkyrienskies.addon.control.block.multiblocks.TileEntityGiantPropellerPart;
-import org.valkyrienskies.addon.control.block.multiblocks.TileEntityRudderPart;
-import org.valkyrienskies.addon.control.block.multiblocks.TileEntityValkyriumCompressorPart;
-import org.valkyrienskies.addon.control.block.multiblocks.TileEntityValkyriumEnginePart;
 import org.valkyrienskies.addon.control.config.VSControlConfig;
 import org.valkyrienskies.addon.control.tileentity.TileEntityGearbox;
 import org.valkyrienskies.addon.control.util.BaseItem;
-import org.valkyrienskies.mod.common.config.VSConfig;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemVSWrench extends BaseItem {
     private EnumWrenchMode mode = EnumWrenchMode.CONSTRUCT;
@@ -45,7 +38,13 @@ public class ItemVSWrench extends BaseItem {
             itemInformation.add(TextFormatting.BLUE + I18n.format("tooltip.vs_control.wrench_toggle"));
         } else {
             itemInformation.add(TextFormatting.BLUE + I18n.format("tooltip.vs_control.wrench." + this.mode.toString()));
-            itemInformation.add(TextFormatting.GREEN + "" + TextFormatting.ITALIC + I18n.format("tooltip.vs_control.wrench_modes"));
+
+            GameSettings settings = Minecraft.getMinecraft().gameSettings;
+            String useItemKey = settings.keyBindUseItem.getDisplayName();
+            String sneakKey = settings.keyBindSneak.getDisplayName();
+
+            itemInformation.add(TextFormatting.GREEN + "" + TextFormatting.ITALIC +
+                    I18n.format("tooltip.vs_control.wrench_modes", sneakKey, useItemKey));
         }
     }
 
